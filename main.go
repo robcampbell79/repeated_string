84e31468cc00bbcf0343f9ffbc2119ebdef3602d
Higalgo <rhc91310@gmail.com>
2019-02-24 15:41:15 -0600

    Update repeatedString func

--- main.go
+++ main.go
@@ -2,12 +2,44 @@ package main
 
 import(
     "fmt"
+    "bufio"
+    "os"
 )
 
 func repeatedString(s string, n int64) int64 {
     
+    count := int64(0)
+
+    for i := 0; i < len(s); i++ {
+        if s[i] == 'a' {
+            count++
+        }
+    }
+
+    count = count * (n / int64(len(s)))
+
+    for i := int64(0); i < n % int64(len(s)); i++ {
+        if s[i] == 'a' {
+            count++
+        }
+    }
+
+    return count
 }
 
 func main() {
+
+    var n int64
+    fmt.Println("Enter string length and string with a space seprating them: ")
+
+    fmt.Scan(&n)
+
+    scanner := bufio.NewReader(os.Stdin)
+    s, _ := scanner.ReadString('\n')
+
+    result := repeatedString(s, n)
+
+    fmt.Println(result)
+
 
 }
